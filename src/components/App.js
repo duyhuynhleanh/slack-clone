56ba2e4d152f01f2a314a7f3f2681e2a8e742787
duyhuynhleanh <n17dccn183@student.ptithcm.edu.vn>
2021-06-27 20:21:39 +0700

    update chuc nang hien nhung nguoi chat nhieu nhat

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import SidePanel from './SidePanel/SidePanel'
 import Messages from './Messages/Messages'
 import MetaPanel from './MetaPanel/MetaPanel'
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns='equal' className='app' style={{ background: '#eee' }}>
     <ColorPanel />
     <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
@@ -22,7 +22,12 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => (
     </Grid.Column>
 
     <Grid.Column width={4}>
-      <MetaPanel />
+      <MetaPanel
+        key={currentChannel && currentChannel.id}
+        userPosts={userPosts}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+      />
     </Grid.Column>
   </Grid>
 )
@@ -31,6 +36,7 @@ const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
   isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts,
 })
 
 export default connect(mapStateToProps)(App)
--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,96 @@
 import React from 'react'
+import {
+  Segment,
+  Accordion,
+  Header,
+  Icon,
+  Image,
+  List,
+} from 'semantic-ui-react'
 
 class MetaPanel extends React.Component {
+  state = {
+    channel: this.props.currentChannel,
+    privateChannel: this.props.isPrivateChannel,
+    activeIndex: 0,
+  }
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+    this.setState({ activeIndex: newIndex })
+  }
+
+  displayTopPosters = (posts) =>
+    Object.entries(posts)
+      .sort((a, b) => b[1] - a[1])
+      .map(([key, val], i) => (
+        <List.Item key={i}>
+          <Image avatar src={val.avatar} />
+          <List.Content>
+            <List.Header as='a'>{key}</List.Header>
+            <List.Description>{val.count} bài đăng</List.Description>
+          </List.Content>
+        </List.Item>
+      ))
+      .slice(0, 5)
+
   render() {
-    return <div>MetaPanel</div>
+    const { activeIndex, privateChannel, channel } = this.state
+    const { userPosts } = this.props
+    if (privateChannel || !channel) return null
+
+    return (
+      <Segment>
+        <Header as='h3' attached='top'>
+          Thông tin về kênh chat #{channel.name}
+        </Header>
+        <Accordion styled attached='true'>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='info' />
+            Chi tiết kênh chat
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            {channel.details}
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='user circle' />
+            Người đóng góp nhiều nhất
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <List>{userPosts && this.displayTopPosters(userPosts)}</List>
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='pencil alternate' />
+            Được tạo bởi
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            <Header as='h3'>
+              <Image circular src={channel.createdBy.avatar} />
+              {channel.createdBy.name}
+            </Header>
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    )
   }
 }
 
