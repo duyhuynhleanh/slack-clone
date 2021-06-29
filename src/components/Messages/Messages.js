702307e22299412eb24e91efbc86f4c556fb50ac
duyhuynhleanh <n17dccn183@student.ptithcm.edu.vn>
2021-06-29 20:02:55 +0700

    update chuc nang doi avatar va hien thi trang thai dang chat...

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -6,6 +6,7 @@ import { setUserPosts } from '../../actions'
 import MessagesHeader from './MessagesHeader'
 import MessageForm from './MessageForm'
 import Message from './Message'
+import Typing from './Typing'
 
 class Messages extends React.Component {
   state = {
@@ -22,6 +23,9 @@ class Messages extends React.Component {
     searchTerm: '',
     searchLoading: false,
     searchResults: [],
+    typingRef: firebase.database().ref('typing'),
+    connectedRef: firebase.database().ref('.info/connected'),
+    typingUsers: [],
   }
 
   componentDidMount() {
@@ -35,6 +39,42 @@ class Messages extends React.Component {
 
   addListeners = (channelId) => {
     this.addMessageListener(channelId)
+    this.addTypingListeners(channelId)
+  }
+
+  addTypingListeners = (channelId) => {
+    let typingUsers = []
+    this.state.typingRef.child(channelId).on('child_added', (snap) => {
+      if (snap.key !== this.state.user.uid) {
+        typingUsers = typingUsers.concat({
+          id: snap.key,
+          name: snap.val(),
+        })
+        this.setState({ typingUsers })
+      }
+    })
+
+    this.state.typingRef.child(channelId).on('child_removed', (snap) => {
+      const index = typingUsers.findIndex((user) => user.id === snap.key)
+      if (index !== -1) {
+        typingUsers = typingUsers.filter((user) => user.id !== snap.key)
+        this.setState({ typingUsers })
+      }
+    })
+
+    this.state.connectedRef.on('value', (snap) => {
+      if (snap.val() === true) {
+        this.state.typingRef
+          .child(channelId)
+          .child(this.state.user.uid)
+          .onDisconnect()
+          .remove((err) => {
+            if (err !== null) {
+              console.error(err)
+            }
+          })
+      }
+    })
   }
 
   addMessageListener = (channelId) => {
@@ -171,6 +211,17 @@ class Messages extends React.Component {
       : ''
   }
 
+  displayTypingUsers = (users) =>
+    users.length > 0 &&
+    users.map((user) => (
+      <div
+        style={{ display: 'flex', alignItems: 'center', marginBottom: '0.2em' }}
+        key={user.id}
+      >
+        <span className='user__typing'>{user.name} đang chat</span> <Typing />
+      </div>
+    ))
+
   render() {
     const {
       messagesRef,
@@ -183,6 +234,7 @@ class Messages extends React.Component {
       searchLoading,
       privateChannel,
       isChannelStarred,
+      typingUsers,
     } = this.state
 
     return (
@@ -196,11 +248,13 @@ class Messages extends React.Component {
           handleStar={this.handleStar}
           isChannelStarred={isChannelStarred}
         />
+
         <Segment>
           <Comment.Group className='messages'>
             {searchTerm
               ? this.displayMessages(searchResults)
               : this.displayMessages(messages)}
+            {this.displayTypingUsers(typingUsers)}
           </Comment.Group>
         </Segment>
 
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,37 @@
 import React from 'react'
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react'
+import {
+  Grid,
+  Header,
+  Icon,
+  Dropdown,
+  Image,
+  Modal,
+  Input,
+  Button,
+} from 'semantic-ui-react'
 import firebase from '../../firebase'
+import AvatarEditor from 'react-avatar-editor'
 // import { connect } from 'react-redux'
 
 class UserPanel extends React.Component {
   state = {
     user: this.props.currentUser,
+    modal: false,
+    previewImage: '',
+    croppedImage: '',
+    blob: '',
+    storageRef: firebase.storage().ref(),
+    userRef: firebase.auth().currentUser,
+    usersRef: firebase.database().ref('users'),
+    metadata: {
+      contentType: 'image/jpeg',
+    },
+    uploadedCropImage: '',
   }
+
+  openModal = () => this.setState({ modal: true })
+
+  closeModal = () => this.setState({ modal: false })
 
   dropdownOptions = () => [
     {
@@ -20,13 +45,50 @@ class UserPanel extends React.Component {
     },
     {
       key: 'avatar',
-      text: <span>Đổi ảnh đại diện</span>,
+      text: <span onClick={this.openModal}>Đổi ảnh đại diện</span>,
     },
     {
       key: 'signout',
       text: <span onClick={this.handleSignout}>Đăng xuất</span>,
     },
   ]
+
+  uploadCropImage = () => {
+    const { storageRef, userRef, blob, metadata } = this.state
+    storageRef
+      .child(`avatar/user-${userRef.uid}`)
+      .put(blob, metadata)
+      .then((snap) => {
+        snap.ref.getDownloadURL().then((downloadURL) => {
+          this.setState({ uploadedCropImage: downloadURL }, () =>
+            this.changeAvatar()
+          )
+        })
+      })
+  }
+
+  changeAvatar = () => {
+    this.state.userRef
+      .updateProfile({
+        photoURL: this.state.uploadedCropImage,
+      })
+      .then(() => {
+        console.log('photo url updated')
+        this.closeModal()
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+    this.state.usersRef
+      .child(this.state.user.uid)
+      .update({ avatar: this.state.uploadedCropImage })
+      .then(() => {
+        console.log('user avatar updated')
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
 
   handleSignout = () => {
     firebase
@@ -35,8 +97,31 @@ class UserPanel extends React.Component {
       .then(() => console.log('signed out'))
   }
 
+  handleChange = (event) => {
+    const file = event.target.files[0]
+    const reader = new FileReader()
+    if (file) {
+      reader.readAsDataURL(file)
+      reader.addEventListener('load', () => {
+        this.setState({ previewImage: reader.result })
+      })
+    }
+  }
+
+  handleCropImage = () => {
+    if (this.avatarEditor) {
+      this.avatarEditor.getImageScaledToCanvas().toBlob((blob) => {
+        let imageUrl = URL.createObjectURL(blob)
+        this.setState({
+          croppedImage: imageUrl,
+          blob,
+        })
+      })
+    }
+  }
+
   render() {
-    const { user } = this.state
+    const { user, modal, previewImage, croppedImage } = this.state
     const { primaryColor } = this.props
 
     return (
@@ -59,6 +144,61 @@ class UserPanel extends React.Component {
               />
             </Header>
           </Grid.Row>
+
+          <Modal basic open={modal} onClose={this.closeModal}>
+            <Modal.Header>Đổi avatar</Modal.Header>
+            <Modal.Content>
+              <Input
+                onChange={this.handleChange}
+                fluid
+                type='file'
+                label='Avatar mới'
+                name='previewImage'
+              />
+              <Grid centered stackable columns={2}>
+                <Grid.Row centered>
+                  <Grid.Column className='ui center aligned grid'></Grid.Column>
+                  {previewImage && (
+                    <AvatarEditor
+                      ref={(node) => (this.avatarEditor = node)}
+                      image={previewImage}
+                      width={120}
+                      height={120}
+                      border={50}
+                      scale={1.2}
+                    />
+                  )}
+                  <Grid.Column className='ui center aligned grid'></Grid.Column>
+                  {croppedImage && (
+                    <Image
+                      style={{ margin: '3.5em auto' }}
+                      width={100}
+                      height={100}
+                      src={croppedImage}
+                    />
+                  )}
+                </Grid.Row>
+              </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              {croppedImage && (
+                <Button color='green' inverted onClick={this.uploadCropImage}>
+                  <Icon name='save' />
+                  Đổi Avatar
+                </Button>
+              )}
+
+              <Button color='green' inverted onClick={this.handleCropImage}>
+                <Icon name='image' />
+                Xem trước
+              </Button>
+
+              <Button color='red' inverted onClick={this.closeModal}>
+                <Icon name='remove' />
+                Hủy
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </Grid.Column>
       </Grid>
     )
